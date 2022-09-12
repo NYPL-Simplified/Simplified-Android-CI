a5d05cc19114c353f6579642236731fd8976a0fc
Rob Neumann <consultrneumann@nypl.org>
2022-09-12 14:03:22 -0700

    Force tests to run despite up-to-date

--- ci-test.sh
+++ ci-test.sh
@@ -37,7 +37,7 @@ case ${BUILD_TYPE} in
       -Dorg.gradle.jvmargs="${JVM_ARGUMENTS}" \
       -Dorg.gradle.parallel=true \
       -Dorg.gradle.internal.publish.checksums.insecure=true \
-      testDebug -x :simplified-tests:testDebug || fatal "could not test"
+      cleanTest testDebug -x :simplified-tests:testDebug || fatal "could not test"
     ;;
 
   release)
